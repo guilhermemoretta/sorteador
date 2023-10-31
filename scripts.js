3ebf480f5192d3da5c5c1f52900ea6679f3d25e5
guilhermemoretta <guilherme.moretta14@gmailcom>
2023-10-30 22:01:01 -0300

    Alteração no JS - Operadores de comparação

--- scripts.js
+++ scripts.js
@@ -3,7 +3,14 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector(".input-min").value) //arredonda qualquer número que chegou aqui
     const max = Math.floor(document.querySelector(".input-max").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min; //pegou um número aleatório, fez as contas e arredondou os resultados
-
-    alert(result)
+       
+    if(min >= max) {
+        alert('O valor minimo term que ser MENOR que o valor máximo')
+    } else {
+      const result = Math.floor(Math.random() * (max - min + 1)) + min; //pegou um número aleatório, fez as contas e arredondou os resultados  
+     
+      alert(result)
+    
+    }
+    
 }
